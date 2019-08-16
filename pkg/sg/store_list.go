07f016c5f2042a652791ae8f07f662d6bb22065c
weibh <weibaohui@gmail.com>
2019-08-16 11:25:25 +0800

    存储列表

--- pkg/sg/store_list.go
+++ pkg/sg/store_list.go
@@ -33,7 +33,7 @@ type ZkServers struct {
 //登录cookie
 //https://192.168.3.60:6080/install/getStorageSystemsOverview.action?cmd_id=0.4124075744799065&user_name=optadmin
 func (r *Robot) StoreList() (*StoreList, error) {
-	url := r.fullURL("/install/getStorageSystemsOverview.action?cmd_id=0.4124075744799065&user_name=" + r.Username)
+	url := r.fullURL("/install/getStorageSystemsOverview.action?user_name=" + r.Username)
 
 	j, err := r.PostWithLoginSession(url, nil)
 	if err != nil {
