a4c70da1782cad1e14c72daa5ff9e36bbc139081
weibh <weibaohui@gmail.com>
2019-08-19 18:14:48 +0800

    注释

--- pkg/api/nfs.go
+++ pkg/api/nfs.go
@@ -1,9 +1,12 @@
 package api
 
 type NFSApi interface {
+	//创建目录
 	CreateDirectory(path string) (ok bool, err error)
+	//删除目录
 	DeleteDirectory(path string) (ok bool, err error)
-
+	//创建配额
 	CreateQuota(path string, ips, ops, readBw, writeBw int) (ok bool, quotaID string, err error)
+	//删除配额
 	DeleteQuota(id string) (ok bool, err error)
 }
