8fd73f5addd73dd7128c6b15c87aa49932c2aade
weibh <weibaohui@gmail.com>
2019-08-20 15:51:53 +0800

    增加重置次数

--- pkg/sg/common/http.go
+++ pkg/sg/common/http.go
@@ -7,34 +7,33 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func (i *Instance) PostWithLoginSession(fullURL string, params map[string]string) (str string, err error) {
 	req := httpkit.Post(fullURL)
 	SetSkipSSLVerify(req)
-
 	for _, v := range i.loginCookies {
 		req.SetCookie(v)
 	}
 	for k, v := range params {
 		req.Param(k, v)
 	}
-
 	req.Header("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	str, err = req.String()
 	if err != nil {
 		return "", err
 	}
 	if strings.Contains(str, "<script>window.top.location='/'</script>") {
-		if i.retryLogin <= 3 {
-			log.Println("进行重新登录", i.retryLogin)
+		if i.retryLogin < 3 {
 			//自动进行重新登录
+			log.Println("进行重新登录", i.retryLogin)
 			i.retryLogin += 1
 			i.connect()
+			time.Sleep(time.Second * 1)
 			return i.PostWithLoginSession(fullURL, params)
 		}
-
-		return "", errors.New("cookie失效，请检查登录参数")
+		return "", errors.New("cookie失效,重试后无效，请检查登录参数")
 	}
 	//重置重新登录次数
 	if i.retryLogin > 0 {
